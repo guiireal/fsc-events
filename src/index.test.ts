ef3aee3067d8f1b05d855a0c506fb101999a3f9d
Guilherme França <guii_real@hotmail.com>
2026-01-23 02:43:49 -0300

    feat(tests): add comprehensive tests for the POST /events endpoint to validate event creation and error handling chore(commitlint): add body-max-line-length rule to enforce consistent commit message formatting fix(package.json): add missing newline at the end of the file for proper formatting feat(index.ts): implement POST /events endpoint with validation for event data

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,95 @@
-describe("Sample Test Suite", () => {
-  test("Sample Test Case", () => {
-    expect(true).toBe(true);
+import axios from "axios";
+
+axios.defaults.validateStatus = () => true;
+
+describe("POST /events", () => {
+  it("should create a new event with valid data", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: 1000,
+      latitude: -90,
+      longitude: -180,
+      date: new Date().setHours(new Date().getHours() + 1),
+      owner_id: crypto.randomUUID()
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should return 400 for invalid owner_id", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: 1000,
+      latitude: -90,
+      longitude: -180,
+      date: new Date().setHours(new Date().getHours() + 1),
+      owner_id: "invalid-uuid"
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 400 for negative ticket_price_in_cents", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: -1000,
+      latitude: -90,
+      longitude: -180,
+      date: new Date().setHours(new Date().getHours() + 1),
+      owner_id: crypto.randomUUID()
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 400 for invalid latitude", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: 1000,
+      latitude: -100,
+      longitude: -180,
+      date: new Date().setHours(new Date().getHours() + 1),
+      owner_id: crypto.randomUUID()
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 400 for invalid longitude", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: 1000,
+      latitude: -90,
+      longitude: -200,
+      date: new Date().setHours(new Date().getHours() + 1),
+      owner_id: crypto.randomUUID()
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should return 400 for past date", async () => {
+    const input = {
+      name: "FSC presencial",
+      ticket_price_in_cents: 1000,
+      latitude: -90,
+      longitude: -180,
+      date: new Date().setHours(new Date().getHours() - 2),
+      owner_id: crypto.randomUUID()
+    };
+
+    const response = await axios.post("http://localhost:3000/events", input);
+
+    expect(response.status).toBe(400);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import { drizzle } from "drizzle-orm/node-postgres";
 import express from "express";
+import * as schema from "./db/schema.js";
 
 const app = express();
 
@@ -6,8 +8,57 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const db = drizzle(process.env.DATABASE_URL!, { schema });
+
+app.post("/events", async (req, res) => {
+  const {
+    name,
+    ticket_price_in_cents: ticketPriceInCents,
+    latitude,
+    longitude,
+    date,
+    owner_id: ownerId
+  } = req.body;
+
+  if (
+    !/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(
+      ownerId
+    )
+  ) {
+    return res.status(400).json({ error: "Invalid owner_id" });
+  }
+
+  if (ticketPriceInCents < 0) {
+    return res
+      .status(400)
+      .json({ error: "The ticket_price_in_cents must be non-negative" });
+  }
+
+  if (latitude < -90 || latitude > 90) {
+    return res.status(400).json({ error: "Invalid latitude" });
+  }
+
+  if (longitude < -180 || longitude > 180) {
+    return res.status(400).json({ error: "Invalid longitude" });
+  }
+
+  const now = new Date();
+  const dateFormatted = new Date(date);
+
+  if (dateFormatted < now) {
+    return res.status(400).json({ error: "The date must be in the future" });
+  }
+
+  const event = await db.insert(schema.eventsTable).values({
+    name,
+    ticketPriceInCents,
+    date: dateFormatted,
+    latitude,
+    longitude,
+    ownerId
+  });
+
+  return res.status(201).json(event);
 });
 
 app.listen(PORT, () => {
