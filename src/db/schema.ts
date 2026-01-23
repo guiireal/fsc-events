49f089626e5de9a4d9643bd462b32a6f85618d35
Guilherme França <guii_real@hotmail.com>
2026-01-23 02:02:46 -0300

    feat: add drizzle-kit

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,18 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
-import { integer } from "node_modules/drizzle-orm/pg-core/index.cjs";
+import {
+  decimal,
+  integer,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar
+} from "drizzle-orm/pg-core";
 
 export const eventsTable = pgTable("events", {
   id: uuid().primaryKey().defaultRandom(),
+  ownerId: uuid("owner_id").notNull(),
   name: varchar({ length: 100 }).notNull(),
-  ticketPriceInCents: integer("ticket_price_in_cents").notNull()
+  ticketPriceInCents: integer("ticket_price_in_cents").notNull(),
+  latitude: decimal({ precision: 9, scale: 6 }).notNull(),
+  longitude: decimal({ precision: 9, scale: 6 }).notNull(),
+  date: timestamp({ withTimezone: true }).notNull()
 });
-
-export const usersTable = pgTable("users", {
-  id: uuid()
-});
-
-// paramos no timezone
